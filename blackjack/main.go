5a0d83300984b531cbe3f1a2989bfa4921ccfb6f
blindstrom00 <108852387+blindstrom00@users.noreply.github.com>
2022-07-11 22:00:42 -0400

    Fixing go.mod issues

---
+++ blackjack/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/blindstrom00/golang-portfolio/tree/main/deck"
+)
+
+func main() {
+	cards := deck.New(deck.Deck(4), deck.Shuffle)
+	var card deck.Card
+	for i := 0; i < 10; i++ {
+		card, cards = cards[0], cards[1:]
+		fmt.Println(card)
+	}
+}
