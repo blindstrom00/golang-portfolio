ddbffa3537cea0a9e0b612f0f339ab9aaf13a4d2
blindstrom00 <108852387+blindstrom00@users.noreply.github.com>
2022-07-11 22:46:24 -0400

    Simple blackjack completed
    - Created the base blackjack game with only hit or stand logic.
    - One dealer card is hidden until you stand
    - In this version players must determine if they have won or lost.
    - There is no dealer AI (yet) so the dealer will only have the two base cards

--- blackjack/main.go
+++ blackjack/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/blindstrom00/deck"
 )
@@ -9,8 +10,44 @@ import (
 func main() {
 	cards := deck.New(deck.Deck(4), deck.Shuffle)
 	var card deck.Card
-	for i := 0; i < 10; i++ {
-		card, cards = cards[0], cards[1:]
-		fmt.Println(card)
+	var player, dealer Hand
+	for i := 0; i < 2; i++ {
+		for _, hand := range []*Hand{&player, &dealer} {
+			card, cards = draw(cards)
+			*hand = append(*hand, card)
+		}
 	}
+	var input string
+	for input != "s" {
+		fmt.Println("Player:", player)
+		fmt.Println("Dealer:", dealer.DealerString())
+		fmt.Println("What would you like to (h)it or (s)tand?")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, cards = draw(cards)
+			player = append(player, card)
+		}
+	}
+	fmt.Println("-- Final Hands --")
+	fmt.Println("Player:", player)
+	fmt.Println("Dealer:", dealer)
+}
+
+type Hand []deck.Card
+
+func (h Hand) String() string {
+	strs := make([]string, len(h))
+	for i := range h {
+		strs[i] = h[i].String()
+	}
+	return strings.Join(strs, ", ")
+}
+
+func (h Hand) DealerString() string {
+	return h[0].String() + ", HIDDEN"
+}
+
+func draw(cards []deck.Card) (deck.Card, []deck.Card) {
+	return cards[0], cards[1:]
 }
